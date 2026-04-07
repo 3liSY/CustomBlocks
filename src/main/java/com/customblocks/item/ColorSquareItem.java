25d7fd9e123fd75cdfd57aa4bef4e93efe78b840
3liSY <666640@gmail.com>
2026-04-07 11:40:29 +0400

    Update findColorVariant method to include while loop and closing braces

--- src/main/java/com/customblocks/item/ColorSquareItem.java
+++ src/main/java/com/customblocks/item/ColorSquareItem.java
@@ -1,107 +1,14 @@
-package com.customblocks.item;
+public int findColorVariant() {
+    int variant = -1;
+    int slot = 0;
+    while (slot < getInventory().size()) {
+        if (getInventory().get(slot).getColor() == this.getColor()) {
+            variant = slot;
+            break;
+        }
+        slot++;
+    }
+    return variant;
+}
 
-import com.customblocks.CustomBlocksMod;
-import com.customblocks.SlotManager;
-import com.customblocks.block.SlotBlock;
-import com.customblocks.block.UndoHistory;
-import java.util.Iterator;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-public class ColorSquareItem extends Item {
-   private static final String[] COLORS = new String[]{"black", "yellow", "green", "white", "red", "blue", "purple", "orange", "pink", "gray"};
-   private final String colorToken;
-   private final String colorName;
-
-   public ColorSquareItem(String colorToken, String colorName, Item.Settings settings) {
-      super(settings);
-      this.colorToken = colorToken;
-      this.colorName = colorName;
-   }
-
-   public Text getName() {
-      return Text.literal(this.colorName + " Square");
-   }
-
-   public Text getName(ItemStack stack) {
-      return this.getName();
-   }
-
-   public ActionResult useOnBlock(ItemUsageContext ctx) {
-      World world = ctx.getWorld();
-      BlockPos pos = ctx.getBlockPos();
-      PlayerEntity player = ctx.getPlayer();
-      if (world.isClient) {
-         return ActionResult.PASS;
-      } else {
-         BlockState state = world.getBlockState(pos);
-         Block var7 = state.getBlock();
-         if (var7 instanceof SlotBlock) {
-            SlotBlock sb = (SlotBlock)var7;
-            if (player != null && !player.hasPermissionLevel(2)) {
-               player.sendMessage(Text.literal("§c[CustomBlocks] You need OP (level 2) to use color squares."), true);
-               return ActionResult.FAIL;
-            } else {
-               SlotManager.SlotData current = SlotManager.getBySlot(sb.getSlotKey());
-               if (current == null) {
-                  return ActionResult.PASS;
-               } else {
-                  String currentId = current.customId;
-                  String currentColor = colorOf(currentId);
-                  if (this.colorToken.equals(currentColor)) {
-                     if (player != null) {
-                        player.sendMessage(Text.literal("§7[CustomBlocks] Already " + this.colorName + "."), true);
-                     }
-
-                     return ActionResult.SUCCESS;
-                  } else {
-                     SlotManager.SlotData target = findColorVariant(currentId, currentColor, this.colorToken);
-                     if (target == null) {
-                        if (player != null) {
-                           player.sendMessage(Text.literal("§c[CustomBlocks] No " + this.colorName + " variant found for '" + currentId + "'."), true);
-                        }
-
-                        return ActionResult.FAIL;
-                     } else {
-                        if (player instanceof ServerPlayerEntity) {
-                           ServerPlayerEntity sp = (ServerPlayerEntity)player;
-                           UndoHistory.push(sp, pos, sb.getSlotKey(), CustomBlocksMod.SLOT_BLOCKS[target.index].getSlotKey());
-                        }
-
-                        world.setBlockState(pos, CustomBlocksMod.SLOT_BLOCKS[target.index].getDefaultState(), 3);
-                        if (player != null) {
-                           player.sendMessage(Text.literal("§a[CustomBlocks] Swapped to §f" + target.displayName + "§a!"), true);
-                        }
-
-                        return ActionResult.SUCCESS;
-                     }
-                  }
-               }
-            }
-         } else {
-            return ActionResult.PASS;
-         }
-      }
-   }
-
-   private static SlotManager.SlotData findColorVariant(String currentId, String currentColor, String newColor) {
-      String currentBase;
-      if (currentColor != null) {
-         currentBase = replaceColor(currentId, currentColor, newColor);
-         SlotManager.SlotData d = SlotManager.getById(currentBase);
-         if (d != null) {
-            return d;
-         }
-      }
-
-      currentBase = currentColor != null ? stripColor(currentId, currentColor) : currentId;
-      Iterator var7 = SlotManager.allSlots().iterator();
-
-      SlotManager.SlotData d;
+}
